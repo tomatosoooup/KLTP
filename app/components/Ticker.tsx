214484819e35d672e336e3725dce04a0282cee08
Vasyl <v.gabinet02@icloud.com>
2023-11-26 15:40:01 +0200

    commit test marquee

--- app/components/Ticker.tsx
+++ app/components/Ticker.tsx
@@ -3,21 +3,21 @@ import Marquee from "react-fast-marquee";
 const Ticker = () => {
   return (
     <>
-      <Marquee autoFill direction="right" className="small-marquee">
+      <Marquee autoFill direction="right" className="small-marquee uppercase">
         <span className="text-xs font-bold font-tt text-[11px]">
           IN CRYPTO WE TRUST
         </span>
       </Marquee>
       <Marquee
         autoFill
-        className="text-5xl lg:text-6xl big-marquee font-cf font-normal"
+        className="text-5xl lg:text-6xl big-marquee font-tt font-black uppercase"
       >
         <span>KNOW ME</span>
         <span>LiKE ME</span>
         <span>TRUST ME</span>
         <span>PAY ME</span>
       </Marquee>
-      <Marquee autoFill direction="right" className="small-marquee">
+      <Marquee autoFill direction="right" className="small-marquee uppercase">
         <span className="text-xs font-bold font-tt rotate-180 text-[11px]">
           IN CRYPTO WE TRUST
         </span>
