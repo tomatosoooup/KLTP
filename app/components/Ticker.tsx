2fe7b1d5013c49359f6039827c644064fb635a6a
Vasyl <v.gabinet02@icloud.com>
2023-11-27 11:17:50 +0200

    fixed marquee

--- app/components/Ticker.tsx
+++ app/components/Ticker.tsx
@@ -10,12 +10,16 @@ const Ticker = () => {
       </Marquee>
       <Marquee
         autoFill
-        className="text-5xl lg:text-6xl big-marquee font-tt font-black uppercase overflow-y-hidden"
+        className="text-5xl lg:text-6xl big-marquee font-cf font-bold uppercase overflow-y-hidden"
       >
         <span>KNOW ME</span>
+        <div className="w-6 h-6 rounded-full bg-white/10"></div>
         <span>LiKE ME</span>
+        <div className="w-6 h-6 rounded-full bg-white/10"></div>
         <span>TRUST ME</span>
+        <div className="w-6 h-6 rounded-full bg-white/10"></div>
         <span>PAY ME</span>
+        <div className="w-6 h-6 rounded-full bg-white/10"></div>
       </Marquee>
       <Marquee autoFill direction="right" className="small-marquee uppercase">
         <span className="text-xs font-bold font-tt rotate-180 text-[11px]">
