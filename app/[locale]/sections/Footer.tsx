38f72e37e5ff6adea60ec6ee67a2f4377ebe0b50
tomatosoooup <you@example.com>
2023-11-30 23:21:13 +0200

    commit

--- app/[locale]/sections/Footer.tsx
+++ app/[locale]/sections/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between mt-10 lg:mt-2 px-5 xl:px-0">
         <div className="absolute min-w-full bg-white/10 h-[2px] top-24 md:top-36 left-0 lg:hidden"></div>
         <div className="absolute min-w-full bg-white/10 h-[2px] bottom-40 md:bottom-48 left-0 lg:hidden"></div>
-        <div className="text-center hidden font-medium text-white pt-4 flex-col font-cf xl:flex">
+        <div className="text-center hidden font-medium text-white pt-16 flex-col font-cf xl:flex">
           <span className="text-5xl 2xl:text-6xl tracking-[0.7rem] pr-2 font-bold">
             KLTP
           </span>
@@ -16,7 +16,7 @@ const Footer = () => {
           </span>
         </div>
         <div className="order-2 lg:order-none relative flex-grow lg:flex-grow-0 lg:ml-3 lg:mt-10">
-          <h4 className="uppercase text-white text-lg font-black text-center lg:text-left mt-8 mb-4 lg:my-0 font-tt">
+          <h4 className="uppercase text-white text-lg font-black text-center lg:text-left mt-8 mb-4 md:my-0 font-tt">
             Локализация
           </h4>
           <div className="flex flex-wrap lg:flex-nowrap gap-6 text-[#D9D9D9] text-sm font-bold md:mt-14">
@@ -50,10 +50,10 @@ const Footer = () => {
           </div>
         </div>
         <div className="order-3 lg:order-none">
-          <h4 className="uppercase text-white text-lg font-black text-center lg:text-left mt-12 lg:mt-0 font-tt pb-4 md:pb-0">
+          <h4 className="uppercase text-white text-lg font-black text-center lg:text-left mt-12 lg:mt-0 font-tt pb-4 md:pb-0 md:pt-6">
             Контакт
           </h4>
-          <div className="flex lg:flex-col gap-20 lg:gap-5 md:mt-14">
+          <div className="flex lg:flex-col gap-20 lg:gap-5 md:mt-20">
             <p className="text-[#D9D9D9] text-base font-light font-ct">
               <Link href={"mailto:kltp.finance@gmail.com?subject=KLTP order"}>
                 kltp.finance@gmail.com
@@ -84,11 +84,11 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="lg:ml-5 order-1 lg:order-none">
+        <div className="order-1 lg:order-none">
           <h4 className="uppercase text-white text-lg font-black pb-4 md:pb-0 text-center lg:text-left font-tt">
             Документы
           </h4>
-          <div className="flex lg:flex-col text-white/70 text-sm gap-10 font-light lg:gap-2 font-ct text-center md:text-left md:mt-8">
+          <div className="flex lg:flex-col text-white/70 text-sm gap-10 font-light lg:gap-2 font-ct text-center md:text-left md:mt-20">
             <Link href={"/"} className="w-full">
               AML / CTF terms
             </Link>
